ab5dbc3656ee7b72add888dc43b6157bb035381a
ahmedjadan <a.jadan7@gmail.com>
2021-09-20 23:04:14 +0200

    meta data

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/globals.css'
 import { CaretProvider } from '@/src/components/context/CartContext'
 import Nprogress from 'nprogress';
 import Router from 'next/router';
+import Head from 'next/head'
 import { SWRConfig } from 'swr'
 import axios from 'axios';
 Router.onRouteChangeStart = () => Nprogress.start();
@@ -14,10 +15,24 @@ Router.onRouteChangeError = () => Nprogress.done();
 const fetcher = url => axios.get(url).then(res => res.data)
 
 function MyApp({ Component, pageProps, }) {
-
+  <Head>
+    <title>El-Dokkan | Home</title>
+    <meta key="charSet" charSet="UTF-8" />
+    <meta name="robots" content="follow, index" />
+    <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1" />
+    <meta key="httpEquiv" httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta key="title" name="title" content="El-Dokkan" />
+    <meta key="description" name="description" content="modern e-commerce small shop, using Strapi for backend, Nextjs for frontend and payment integration with PayPal, " />
+    <meta key="keywords" name="keywords" content="shop, dokkan, e-commerce" />
+    <meta key="author" name="author" content="Ahmed Jadan" />
+    <meta key="article:published_time" property="article:published_time" content={new Date(date).toISOString()} />
+    <meta key="og:title" property="og:title" content={"El-Dokkan | E-commerce" || "By Ahmed Ja'dan"} />
+    <meta key="og:description" property="og:description" content="modern e-commerce small shop, using Strapi for backend, Nextjs for frontend and payment integration with PayPal," />
+    <meta key="og:type" property="og:type" content="website" />
+  </Head>
   return (
     <CaretProvider>
-      <SWRConfig value={{fetcher}}>
+      <SWRConfig value={{ fetcher }}>
         <Component {...pageProps} />
       </SWRConfig>
     </CaretProvider>
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import ProductItem from '@/src/components/ProductItem'
 //
 
 
-export default function index({products}) {
- 
+export default function index({ products }) {
+
   if (!products) {
     return <Layout> <div className="max-w-7xl mx-auto text-center">  </div> </Layout>
   }
@@ -14,10 +14,22 @@ export default function index({products}) {
     <Layout>
       <Head>
         <title>El-Dokkan | Home</title>
+        <meta key="charSet" charSet="UTF-8" />
+        <meta name="robots" content="follow, index" />
+        <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1" />
+        <meta key="httpEquiv" httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta key="title" name="title" content="El-Dokkan" />
+        <meta key="description" name="description" content="modern e-commerce small shop, using Strapi for backend, Nextjs for frontend and payment integration with PayPal, " />
+        <meta key="keywords" name="keywords" content="shop, dokkan, e-commerce" />
+        <meta key="author" name="author" content="Ahmed Jadan" />
+        <meta key="article:published_time" property="article:published_time" content={new Date(date).toISOString()} />
+        <meta key="og:title" property="og:title" content={"El-Dokkan | E-commerce" || "By Ahmed Ja'dan"} />
+        <meta key="og:description" property="og:description" content="modern e-commerce small shop, using Strapi for backend, Nextjs for frontend and payment integration with PayPal," />
+        <meta key="og:type" property="og:type" content="website" />
       </Head>
       <div>
-        <ProductItem products={products}  />
-     
+        <ProductItem products={products} />
+
       </div>
     </Layout>
   )
